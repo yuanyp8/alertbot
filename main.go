51b4cb4ecbf980e3beafac305f2ac137c9bdafbf
yuanyp8 <51024420+yuanyp8@users.noreply.github.com>
2022-07-11 16:21:24 +0800

    Update main.go

--- main.go
+++ main.go
@@ -7,9 +7,9 @@ import (
 )
 
 func main() {
-	_ = os.Setenv("CGO_ENABLED", "0")
-	_ = os.Setenv("GOARCH", "amd64")
-	_ = os.Setenv("GOOS", "linux")
+// 	_ = os.Setenv("CGO_ENABLED", "0")
+// 	_ = os.Setenv("GOARCH", "amd64")
+// 	_ = os.Setenv("GOOS", "linux")
 
 	if err := cmd.RootCmd.Execute(); err != nil {
 		fmt.Println(err)
